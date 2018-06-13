c5aea14b776d1a1ee04dc347a3345907f9eec73f
Shrung Bhatt <huntjigsaw97@gmail.com>
2018-06-13 10:33:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/projects/shrungbhatt/blitzzardemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.projects.shrungbhatt.blitzzardemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
